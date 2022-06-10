87a2f8e5a90e9c4c307645b2651610d8bb5bfa29
ChanhyukPark-Tech <pgg6713@gmail.com>
2022-06-10 16:54:55 +0900

    chore: styled-component dependency setting

--- packages/defaultui/src/components/Button/Button.tsx
+++ packages/defaultui/src/components/Button/Button.tsx
@@ -1,10 +1,11 @@
 import React from "react";
+import * as Styled from "./Button.style";
 
 export interface ButtonProps {
   label: string;
 }
 const Button = ({ label }: ButtonProps) => {
-  return <div>{label}</div>;
+  return <Styled.Container>{label}</Styled.Container>;
 };
 
 export default Button;
